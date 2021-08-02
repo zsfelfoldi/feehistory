27136e1db4d5f6a74117a98944b567dad867da62
Zsolt Felfoldi <zsfelfoldi@gmail.com>
2021-08-02 17:03:40 +0200

    updates

--- docs/feeOracle.md
+++ docs/feeOracle.md
@@ -1,4 +1,4 @@
-## Fee Oracle
+## Economical Fee Oracle
 
 Author: Zsolt Felfoldi (zsfelfoldi@ethereum.org)
 
@@ -10,7 +10,36 @@ This "oracle" calculates suggested fees for different time preferences. The func
 
 The application frontend might display the fees vs time factor as a bar graph or curve. The steepness of this curve might also give a hint to users on whether there is currently a local congestion.
 
-### Algorithm description
+### Algorithm
 
-[...]
+The oracle first requests `eth_feeHistory("0x64", "latest")` which is a cheap request (even with a light client backend) because it only requires block headers. The result contains the `baseFee` and `gasUsedRatio` history for the last 100 blocks but no priority fee (effective reward) information. The `baseFee` history is used directly by `suggestBaseFee` to estimate a value under which the `baseFee` is expected to probably fall in the future time period proportional to `timeFactor`. The `gasUsedRatio` is used to find a few recent non-empty and non-full blocks where `suggestPriorityFee` can do another `eth_feeHistory` request, this time also requesting the required `rewardPercentile` in order to make a suggestion for the priority fee.
 
+#### suggestPriorityFee
+
+The 5 most recent non-empty and non-full blocks (`0 < gasUsedRatio <= 0.9`) are selected and the specified `rewardPercentile` of each of those blocks is queried. The resulting reward values are sorted and the final priority fee suggestion is selected from that sorted list based on the `rewardBlockPercentile` value.
+
+The resulting suggestion is on the lower end of the effective rewards of recently included transactions (where that reward has been proven enough). Setting both `rewardPercentile` and `rewardBlockPercentile` to zero will result in the absolute minimum of recent rewards being selected. The default settings are non-zero in order to filter out outliers. A non-zero `rewardPercentile` is supposed to filter out each miner's own transactions with potentially too low rewards in each block while `rewardBlockPercentile` filters out blocks with unusually low miner reward thresholds.
+
+#### predictMinBaseFee
+
+The oracle tries to offer economical fee suggestions based on the assumption that `baseFee` usually fluctuates around a market equilibrium and therefore past `baseFee` history in a given timeframe is a somewhat good indicator of future fluctuation in a similar timeframe. This assumption might not apply when fundamental market conditions are changed but if the initial, economical fee suggestion does not succeed in getting the transaction included then the offered fees can still be increased.
+
+Base fees of full blocks (where `gasUsedRatio > 0.9`) are not considered relevant because getting in those blocks might have required excessive priority fees offsetting the `baseFee` difference compared to the next block. Therefore the base fee history is preprocessed in a way that the base fee of every full block is replaced by that of the next non-full block. The last (pending) block is also assumed to be full and its `baseFee` value is multiplied by `9/8` which yields the assumed `baseFee` of the next block in case the pending one indeed ends up full. This is a worst case estimation that gives some upwards bias to more urgent (low `timeFactor`) suggestions. Finally the pre-processed `baseFee` history is exponentially weighted with `Math.exp(block.age / timeFactor)` and the percentile range between `sampleMinPercentile` and `sampleMaxPercentile` is averaged out with a half-sine time window function.
+
+#### Final fee parameter suggestions
+
+In most cases the base fee suggestions as a funtion of `timeFactor` are monotonically decreasing because the weighted `baseFee` values corresponding to a higher `timeFactor` are selected from a longer history and therefore have higher variance and selecting a low percentile value from a higher variance set yields a higher downwards deviation from the mean. In plain words this means that a fluctuating `baseFee` is expected to reach lower values with a given probability under a longer time period. If the base fees are indeed monotonically decreasing with `timeFactor` then the final suggestions are calculated as
+
+```
+maxPriorityFeePerGas = suggestedPriorityFee		// constant over the entire timeFactor range
+maxFeePerGas = predictedBaseFee[timeFactor] + suggestedPriorityFee
+```
+
+On the other hand, if `predictedBaseFee` increases with `timeFactor` then the `baseFee` is currently unusually low compared to longer term history. In this case we assume that it might rise back soon, meaning that we can't necessarily expect to fit into the next few blocks with the minimum necessary priority fee because there might be a competition for getting into those blocks with a lower base fee. For this reason we define `maxBaseFeeAfter[timeFactor] =  MAX(predictedBaseFee[i])` where `i >= timeFactor` and the final formula looks like this:
+
+```
+maxPriorityFeePerGas = suggestedPriorityFee	+ (maxBaseFeeAfter[timeFactor] - predictedBaseFee[timeFactor]) * extraPriorityFeeRatio
+maxFeePerGas = maxBaseFeeAfter[timeFactor] + suggestedPriorityFee
+```
+
+Note that for `maxFeePerGas` we always offer the longer timeframe estimation if it's higher than the short term one but with the extra priority fee offered we hope that we might still get in earlier with a lower `baseFee` and pay less overall.
--- js/feeOracle.js
+++ js/feeOracle.js
@@ -5,11 +5,15 @@ Author: Zsolt Felfoldi (zsfelfoldi@ethereum.org)
 Description: https://github.com/zsfelfoldi/feehistory/blob/main/docs/feeOracle.md
 */
 
-var sampleMin = 0.1;
-var sampleMax = 0.3;
-var maxTimeFactor = 15;
-var extraTipRatio = 0.25;
-var fallbackTip = 2e9;
+var sampleMinPercentile = 10;	// sampled percentile range of exponentially weighted baseFee history
+var sampleMaxPercentile = 30;
+
+var rewardPercentile = 10;		// effertive reward value to be selected from each individual block
+var rewardBlockPercentile = 40; // suggested priority fee to be selected from sorted individual block reward percentiles
+
+var maxTimeFactor = 15;				// highest timeFactor index in the returned list of suggestion
+var extraPriorityFeeRatio = 0.25;	// extra priority fee offered in case of expected baseFee rise
+var fallbackPriorityFee = 2e9;		// priority fee offered when there are no recent transactions
 
 /*
 suggestFees returns a series of maxFeePerGas / maxPriorityFeePerGas values suggested for different time preferences. The first element corresponds to the highest time preference (most urgent transaction).
@@ -26,7 +30,7 @@ function suggestFees() {
         order.push(i);
     }
 
-    // If a block is full then the baseFee of the next block is copied. The reason is that in full blocks the minimal tip might not be enough to get included.
+    // If a block is full then the baseFee of the next block is copied. The reason is that in full blocks the minimal priority fee might not be enough to get included.
     // The last (pending) block is also assumed to end up being full in order to give some upwards bias for urgent suggestions.
     baseFee[baseFee.length - 1] *= 9 / 8;
     for (var i = feeHistory.gasUsedRatio.length - 1; i >= 0; i--) {
@@ -47,19 +51,19 @@ function suggestFees() {
         return 0;
     })
 
-    var tip = suggestTip(parseInt(feeHistory.oldestBlock, 16), feeHistory.gasUsedRatio);
+    var priorityFee = suggestPriorityFee(parseInt(feeHistory.oldestBlock, 16), feeHistory.gasUsedRatio);
     var result = [];
     var maxBaseFee = 0;
     for (var timeFactor = maxTimeFactor; timeFactor >= 0; timeFactor--) {
-        var bf = suggestBaseFee(baseFee, order, timeFactor);
-        var t = tip;
+        var bf = predictMinBaseFee(baseFee, order, timeFactor);
+        var t = priorityFee;
         if (bf > maxBaseFee) {
             maxBaseFee = bf;
         } else {
             // If a narrower time window yields a lower base fee suggestion than a wider window then we are probably in a price dip.
-            // In this case getting included with a low tip is not guaranteed; instead we use the higher base fee suggestion
-            // and also offer extra tip to increase the chance of getting included in the base fee dip.
-            t += (maxBaseFee - bf) * extraTipRatio;
+            // In this case getting included with a low priority fee is not guaranteed; instead we use the higher base fee suggestion
+            // and also offer extra priority fee to increase the chance of getting included in the base fee dip.
+            t += (maxBaseFee - bf) * extraPriorityFeeRatio;
             bf = maxBaseFee;
         }
         result[timeFactor] = {
@@ -70,8 +74,8 @@ function suggestFees() {
     return result;
 }
 
-// suggestTip suggests a tip (maxPriorityFeePerGas) value that's usually sufficient for blocks that are not full.
-function suggestTip(firstBlock, gasUsedRatio) {
+// suggestPriorityFee suggests a priority fee (maxPriorityFeePerGas) value that's usually sufficient for blocks that are not full.
+function suggestPriorityFee(firstBlock, gasUsedRatio) {
     var ptr = gasUsedRatio.length - 1;
     var needBlocks = 5;
     var rewards = [];
@@ -79,7 +83,7 @@ function suggestTip(firstBlock, gasUsedRatio) {
         var blockCount = maxBlockCount(gasUsedRatio, ptr, needBlocks);
         if (blockCount > 0) {
             // feeHistory API call with reward percentile specified is expensive and therefore is only requested for a few non-full recent blocks.
-            var feeHistory = eth.feeHistory("0x"+blockCount.toString(16), firstBlock + ptr, [0]);
+            var feeHistory = eth.feeHistory("0x"+blockCount.toString(16), firstBlock + ptr, [rewardPercentile]);
             for (var i = 0; i < feeHistory.reward.length; i++) {
                 rewards.push(parseInt(feeHistory.reward[i][0]));
             }
@@ -92,13 +96,13 @@ function suggestTip(firstBlock, gasUsedRatio) {
     }
 
     if (rewards.length == 0) {
-        return fallbackTip;
+        return fallbackPriorityFee;
     }
     rewards.sort();
-    return rewards[Math.floor(rewards.length / 2)];
+    return rewards[Math.floor((rewards.length - 1) * rewardBlockPercentile / 100)];
 }
 
-// maxBlockCount returns the number of consecutive blocks suitable for tip suggestion (gasUsedRatio non-zero and not higher than 0.9).
+// maxBlockCount returns the number of consecutive blocks suitable for priority fee suggestion (gasUsedRatio non-zero and not higher than 0.9).
 function maxBlockCount(gasUsedRatio, ptr, needBlocks) {
     var blockCount = 0;
     while (needBlocks > 0 && ptr >= 0) {
@@ -112,8 +116,8 @@ function maxBlockCount(gasUsedRatio, ptr, needBlocks) {
     return blockCount;
 }
 
-// suggestBaseFee calculates an average of base fees in the sampleMin to sampleMax percentile range of recent base fee history, each block weighted with an exponential time function based on timeFactor.
-function suggestBaseFee(baseFee, order, timeFactor) {
+// predictMinBaseFee calculates an average of base fees in the sampleMinPercentile to sampleMaxPercentile percentile range of recent base fee history, each block weighted with an exponential time function based on timeFactor.
+function predictMinBaseFee(baseFee, order, timeFactor) {
     if (timeFactor < 1e-6) {
         return baseFee[baseFee.length - 1];
     }
@@ -123,7 +127,7 @@ function suggestBaseFee(baseFee, order, timeFactor) {
     var samplingCurveLast = 0;
     for (var i = 0; i < order.length; i++) {
         sumWeight += pendingWeight * Math.exp((order[i] - baseFee.length + 1) / timeFactor);
-        var samplingCurveValue = samplingCurve(sumWeight);
+        var samplingCurveValue = samplingCurve(sumWeight * 100);
         result += (samplingCurveValue - samplingCurveLast) * baseFee[order[i]];
         if (samplingCurveValue >= 1) {
             return result;
@@ -134,13 +138,13 @@ function suggestBaseFee(baseFee, order, timeFactor) {
 }
 
 // samplingCurve is a helper function for the base fee percentile range calculation.
-function samplingCurve(sumWeight) {
-    if (sumWeight <= sampleMin) {
+function samplingCurve(percentile) {
+    if (percentile <= sampleMinPercentile) {
         return 0;
     }
-    if (sumWeight >= sampleMax) {
+    if (percentile >= sampleMaxPercentile) {
         return 1;
     }
-    return (1 - Math.cos((sumWeight - sampleMin) * 2 * Math.PI / (sampleMax - sampleMin))) / 2;
+    return (1 - Math.cos((percentile - sampleMinPercentile) * 2 * Math.PI / (sampleMaxPercentile - sampleMinPercentile))) / 2;
 }
 
